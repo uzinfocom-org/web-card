48711b359c9afc95457009165a99e7b44902bac3
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2025-08-04 08:20:56 +0500

    feat: init

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Web card website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.callPackage ./shell.nix {};
+    });
+}
